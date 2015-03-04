07bf899db97b85dcb10566bcde37c1bc331fe047
Pierre Salmon <pierre.salmon@expandium.com>
2015-03-04 17:46:45 +0100

    Add path description

--- Restd/lib/Restd/Scan/Path.pm
+++ Restd/lib/Restd/Scan/Path.pm
@@ -11,7 +11,13 @@ use File::Spec;
 use Moose;
 use namespace::autoclean;
 
-has 'router_re' => (
+extends 'Restd::Object';
+
+has 'path' => (
+	is => 'rw',
+	isa => 'Str'
+);
+has 'pattern' => (
 	is => 'rw'
 );
 has 'method' => (
@@ -22,9 +28,45 @@ has 'content_type' => (
 	is => 'rw',
 	isa => 'Str'
 );
+has 'handle' => (
+	is => 'rw'
+);
 
 sub BUILD {
 	my $self = shift;
+
+	$self->prepare($o->path());
+}
+
+sub prepare {
+	my $self = shift;
+	my $path = shift;
+
+	my @dirs = File::Spec->splitdir($path);
+	my $pattern = "";
+
+	foreach my $d (@dirs) {
+		if ($d ~= /__(\w+)__/) {
+
+		} else {
+			$pattern .= "";
+		}
+	}
+}
+
+sub eval {
+	my $self = shift;
+	my $path = shift;
+
+	my $pattern = $o->pattern();
+	my $result = 0;
+
+	# Apply pattern
+	if ($path =~ m/$pattern/g) {
+
+	}
+
+	return $result;
 }
 
 __PACKAGE__->meta->make_immutable;
